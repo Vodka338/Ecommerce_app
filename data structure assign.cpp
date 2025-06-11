779da0177d2bfe6e6a4d303c923fe8ad33efddc7
eya777hai <hailueyassu82@gmail.com>
2025-06-11 20:39:29 +0300

    Update data structure assign.cpp

--- data structure assign.cpp
+++ data structure assign.cpp
@@ -35,6 +35,28 @@ void display(const vector<Product>& products) {
     }
 }
 
+// Quick Sort by Price
+int partition(vector<Product>& arr, int low, int high) {
+    float pivot = arr[high].price;
+    int i = low - 1;
+    for (int j = low; j < high; j++) {
+        if (arr[j].price < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quickSort(vector<Product>& arr, int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
 
 
 
